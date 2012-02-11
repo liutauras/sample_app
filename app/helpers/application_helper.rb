d692c0256db78420ea5afe84907358587a8f0889
Valiukas Liutautas <v.liutauras@gmail.com>
2012-02-11 23:13:51 +0200

    Add full_title helper, and implement this helper usage in app/views/layouts/application.html.erb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(title)
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if title.empty?
+      base_title
+    else
+      "#{base_title} | #{title}"
+    end
+  end
 end
