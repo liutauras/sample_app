b1bd8a48d0616faeac6690e7b2525ac038ba9ea3
Valiukas Liutautas <v.liutauras@gmail.com>
2012-02-23 15:43:27 +0200

    Revert "#new file:   app/assets/images/logo.png"
    This reverts commit e7ff70ccd2cada454ee2b7658023e5366c40b85e.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,34 +2,59 @@ require 'spec_helper.rb'
 
 describe "Static pages" do
 
-  subject { page }
-
   describe "Home page" do
-    before { visit root_path }
 
-    it { should have_selector('h1', text: 'Sample App') }
-    it { should have_selector 'title', text: full_title('Home') }
+    it "should have the h1 'Sample App'" do
+      visit root_path
+      page.should have_selector('h1', text: 'Sample App')
+    end
+
+    it "should have the title 'Home'" do
+      visit root_path
+      page.should have_selector('title',
+                        text: "Ruby on Rails Tutorial Sample App | Home")
+    end
   end
 
   describe "Help page" do
-    before { visit help_path }
 
-    it { should have_selector('h1', text: 'Help') }
-    it { should have_selector 'title', text: full_title('Help') }
+    it "should have the h1 'Help'" do
+      visit help_path
+      page.should have_selector('h1', text: 'Help')
+    end
+
+    it "should have the title 'Help'" do
+      visit help_path
+      page.should have_selector('title',
+                        text: "Ruby on Rails Tutorial Sample App | Help")
+    end
   end
 
   describe "About page" do
 
-    before { visit about_path }
+    it "should have the h1 'About'" do
+      visit about_path
+      page.should have_selector('h1', text: 'About Us')
+    end
 
-    it { should have_selector('h1', text: 'About Us') }
-    it { should have_selector 'title', text: full_title('About Us') }
+    it "should have the title 'About Us'" do
+      visit about_path
+      page.should have_selector('title',
+                    text: "Ruby on Rails Tutorial Sample App | About Us")
+    end
   end
 
   describe "Contact page" do
-    before { visit contact_path }
 
-    it { should have_selector('h1',    text: 'Contact') }
-    it { should have_selector('title', text: full_title('Contact')) }
+    it "should have the h1 'Contact'" do
+      visit contact_path
+      page.should have_selector('h1', text: 'Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit contact_path
+      page.should have_selector('title',
+                    text: "Ruby on Rails Tutorial Sample App | Contact")
+    end
   end
 end
